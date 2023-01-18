775c24b8dc77a94c34951f6052a001cb8e34d008
kromych <kromych@users.noreply.github.com>
2023-01-17 23:56:29 -0800

    Generalize the parser

--- corgini/src/lib.rs
+++ corgini/src/lib.rs
@@ -41,6 +41,117 @@
 
 #![cfg_attr(not(test), no_std)]
 
+// Marker trait for ASCII and UTF-8 characters.
+pub trait Char {}
+impl Char for u8 {}
+impl Char for char {}
+
+pub trait CharKind<C: Char> {
+    fn is_whitespace(&self) -> bool;
+    fn is_alpha(&self) -> bool;
+    fn is_digit(&self) -> bool;
+    fn is_underscore(&self) -> bool;
+    fn is_minus(&self) -> bool;
+    fn is_plus(&self) -> bool;
+    fn is_assign(&self) -> bool;
+    fn is_hash(&self) -> bool;
+    fn is_null(&self) -> bool;
+    fn is_newline(&self) -> bool;
+    fn is_quote(&self) -> bool;
+}
+
+impl CharKind<u8> for u8 {
+    fn is_whitespace(&self) -> bool {
+        (b'\t'..=b' ').contains(self)
+    }
+
+    fn is_alpha(&self) -> bool {
+        self.is_ascii_alphabetic()
+    }
+
+    fn is_digit(&self) -> bool {
+        self.is_ascii_digit()
+    }
+
+    fn is_underscore(&self) -> bool {
+        *self == b'_'
+    }
+
+    fn is_plus(&self) -> bool {
+        *self == b'-'
+    }
+
+    fn is_minus(&self) -> bool {
+        *self == b'='
+    }
+
+    fn is_assign(&self) -> bool {
+        *self == b'='
+    }
+
+    fn is_hash(&self) -> bool {
+        *self == b'#'
+    }
+
+    fn is_null(&self) -> bool {
+        *self == 0
+    }
+
+    fn is_newline(&self) -> bool {
+        *self == b'\n'
+    }
+
+    fn is_quote(&self) -> bool {
+        *self == b'"'
+    }
+}
+
+impl CharKind<char> for char {
+    fn is_whitespace(&self) -> bool {
+        ('\t'..=' ').contains(self)
+    }
+
+    fn is_alpha(&self) -> bool {
+        self.is_alphabetic()
+    }
+
+    fn is_digit(&self) -> bool {
+        self.is_numeric()
+    }
+
+    fn is_underscore(&self) -> bool {
+        *self == '_'
+    }
+
+    fn is_minus(&self) -> bool {
+        *self == '-'
+    }
+
+    fn is_plus(&self) -> bool {
+        *self == '+'
+    }
+
+    fn is_assign(&self) -> bool {
+        *self == '='
+    }
+
+    fn is_hash(&self) -> bool {
+        *self == '#'
+    }
+
+    fn is_null(&self) -> bool {
+        *self == '\x00'
+    }
+
+    fn is_newline(&self) -> bool {
+        *self == '\n'
+    }
+
+    fn is_quote(&self) -> bool {
+        *self == '"'
+    }
+}
+
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub struct Location {
     line: usize,
@@ -80,33 +191,45 @@ pub enum Error {
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
-enum Token<'a> {
+enum Token<'a, C>
+where
+    C: Char,
+{
     Unknown(Error),
     Assign,
-    Literal(&'a [u8]),
+    Literal(&'a [C]),
     EndOfInput,
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
-pub struct KeyValue<'a> {
-    pub key: &'a [u8],
-    pub value: &'a [u8],
+pub struct KeyValue<'a, C>
+where
+    C: Char,
+{
+    pub key: &'a [C],
+    pub value: &'a [C],
 }
 
-pub struct Parser<'a> {
+pub struct Parser<'a, C>
+where
+    C: Char,
+{
     location: Location,
-    input: &'a [u8],
+    input: &'a [C],
 }
 
-impl<'a> Parser<'a> {
-    pub fn new(input: &'a [u8]) -> Self {
+impl<'a, C> Parser<'a, C>
+where
+    C: Char + CharKind<C> + Copy,
+{
+    pub fn new(input: &'a [C]) -> Self {
         Self {
             location: Location::default(),
             input,
         }
     }
 
-    fn parse_token(&mut self) -> Token<'a> {
+    fn parse_token(&mut self) -> Token<'a, C> {
         if self.location.pos >= self.input.len() {
             return Token::EndOfInput;
         }
@@ -116,45 +239,42 @@ impl<'a> Parser<'a> {
 
         'outer: while loc.pos < self.input.len() {
             let b = self.input[loc.pos];
-            match b {
-                0 => {
-                    tok = Token::EndOfInput;
-                    break;
-                }
-                b'\n' => loc.new_line(),
-                b'\t'..=b' ' => loc.advance(),
-                b'=' => {
-                    loc.advance();
-                    tok = Token::Assign;
-                    break;
-                }
-                b'#' => {
-                    loc.advance();
-                    while loc.pos < self.input.len() {
-                        if self.input[loc.pos] == b'\n' {
-                            continue 'outer;
-                        }
-                        loc.advance();
+            if b.is_null() {
+                tok = Token::EndOfInput;
+                break;
+            } else if b.is_newline() {
+                loc.new_line();
+            } else if b.is_whitespace() {
+                loc.advance();
+            } else if b.is_assign() {
+                loc.advance();
+                tok = Token::Assign;
+                break;
+            } else if b.is_hash() {
+                loc.advance();
+                while loc.pos < self.input.len() {
+                    if self.input[loc.pos].is_newline() {
+                        continue 'outer;
                     }
+                    loc.advance();
                 }
-                b'0'..=b'9' | b'A'..=b'Z' | b'a'..=b'z' => {
-                    let start_loc = loc;
+            } else if b.is_alpha() || b.is_digit() {
+                let start_loc = loc;
 
-                    loc.advance();
-                    while loc.pos < self.input.len() {
-                        if matches!(self.input[loc.pos], b'0'..=b'9' | b'A'..=b'Z' | b'a'..=b'z') {
-                            loc.advance();
-                        } else {
-                            break;
-                        }
+                loc.advance();
+                while loc.pos < self.input.len() {
+                    let b = self.input[loc.pos];
+                    if b.is_alpha() || b.is_digit() {
+                        loc.advance();
+                    } else {
+                        break;
                     }
-                    tok = Token::Literal(&self.input[start_loc.pos..loc.pos]);
-                    break;
                 }
-                _ => {
-                    tok = Token::Unknown(Error::UnexpectedToken(self.location));
-                    break;
-                }
+                tok = Token::Literal(&self.input[start_loc.pos..loc.pos]);
+                break;
+            } else {
+                tok = Token::Unknown(Error::UnexpectedToken(self.location));
+                break;
             }
         }
 
@@ -162,7 +282,7 @@ impl<'a> Parser<'a> {
         tok
     }
 
-    pub fn parse(&mut self) -> Result<Option<KeyValue<'a>>, Error> {
+    pub fn parse(&mut self) -> Result<Option<KeyValue<'a, C>>, Error> {
         match self.parse_token() {
             Token::EndOfInput => Ok(None),
             Token::Literal(key) => {
@@ -189,7 +309,7 @@ mod tests {
     use crate::Parser;
 
     #[test]
-    fn parse_key_value() {
+    fn parse_key_value_ascii() {
         let input = b"foo = bar";
         let mut parser = Parser::new(input);
         let foo_bar = parser.parse();
@@ -204,9 +324,26 @@ mod tests {
         let eoi = parser.parse();
         assert_eq!(eoi, Ok(None))
     }
+    /*
+        fn parse_key_value() {
+            let input = "foo = bar";
+            let mut parser = Parser::new(input);
+            let foo_bar = parser.parse();
+            assert_eq!(
+                foo_bar,
+                Ok(Some(KeyValue {
+                    key: "foo",
+                    value: "bar"
+                }))
+            );
+
+            let eoi = parser.parse();
+            assert_eq!(eoi, Ok(None))
+        }
+    */
 
     #[test]
-    fn parse_key_values() {
+    fn parse_key_values_ascii() {
         let input =
             b"foo0 = bar0\nfoo1 = bar1\nfoo2 = bar2\nfoo3 = bar3#.....\n#.........\nfoo4 = bar4\n\n";
         let mut parser = Parser::new(input);
