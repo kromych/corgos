69696c6ecc94025e4836d4244666818f64de1d69
kromych <kromych@users.noreply.github.com>
2023-06-17 14:53:28 -0700

    Run cliipy on build

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+cargo clippy --target corgboot/x86_64-boot.json -Zbuild-std=core -Zbuild-std-features=compiler-builtins-mem
+cargo clippy --target corgboot/x86_64-boot.json -Zbuild-std=core -Zbuild-std-features=compiler-builtins-mem
+
 cargo build --target corgboot/x86_64-boot.json -Zbuild-std=core -Zbuild-std-features=compiler-builtins-mem
 cargo build --release --target corgboot/x86_64-boot.json -Zbuild-std=core -Zbuild-std-features=compiler-builtins-mem
